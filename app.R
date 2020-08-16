53f14773c37f76366fb838f30e371c2debf6d4b5
Pawel Zayakin <pz@magisters.net>
2020-08-16 17:23:50 +0000

    alfa

--- app.R
+++ app.R
@@ -7,9 +7,9 @@
 #    http://shiny.rstudio.com/
 #
 timeout<-2147483
-options(shiny.maxRequestSize = 30000*1024^2,port=4444,app_init_timeout=timeout,shiny.app_init_timeout=timeout,
+options(shiny.maxRequestSize = 30000*1024^2,app_init_timeout=timeout,shiny.app_init_timeout=timeout,
         shiny.app_idle_timeout=timeout,app_connection_timeout=timeout,shiny.app_connection_timeout=timeout,
-        app_read_timeout=timeout,shiny.app_read_timeout=timeout,app_idle_timeout=timeout,shiny.trace=T)
+        app_read_timeout=timeout,shiny.app_read_timeout=timeout,app_idle_timeout=timeout) #,port=4444,shiny.trace=T)
 source("shiny/global.R")
 source("shiny/ui.R")
 source("shiny/server.R")
