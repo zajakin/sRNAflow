980f74d2dad779a9250314b165e8f0c69fdcbb4f
Pawel Zayakin <pz@magisters.net>
2020-11-13 22:03:07 +0000

    update_blastdb fix

--- bin/sRNAflow_local_blast_db.R
+++ bin/sRNAflow_local_blast_db.R
@@ -3,5 +3,6 @@ arg<-commandArgs()
 WD<-file.path(arg[length(arg)],"www","db","blast")
 if(!dir.exists(WD)) dir.create(WD,recursive = TRUE, mode = "0777")
 file.remove(file.path(WD,"db.done"))
-system(paste("cd",WD," && update_blastdb --decompress nt"))
+while(system(paste("cd",WD," && update_blastdb --decompress nt"))!=0) next;
 file.create(file.path(WD,"db.done"))
+
